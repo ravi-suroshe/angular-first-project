f7e53d6c9384296ad59e34f4d9a2299d260642fa
Ravi Suroshe <IN000363@idiada.com>
2018-12-21 09:34:47 +0530

    first commit to angular beginner project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] =[
+        {id : 11, name : 'Mr. Nice'},
+        {id : 12, name : 'Mr. Bad'},
+        {id : 13, name : 'Mr. Good'},
+        {id : 14, name : 'Mr. Best'},
+        {id : 15, name : 'Mr. Better'},
+        {id : 16, name : 'Mr. Lucky'},
+        {id : 17, name : 'Mr. Worst'},
+        {id : 18, name : 'Mr. Handsome'},
+        {id : 19, name : 'Mr. Killer'},
+        {id : 20, name : 'Mr. Robost'},
+        {id : 21, name : 'Mr. Sexy'}
+];
+
