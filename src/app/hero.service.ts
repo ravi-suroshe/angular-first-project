08a0168535643694773602acf93b24593d99710e
Ravi Suroshe <IN000363@idiada.com>
2018-12-27 10:44:57 +0530

    Add and Delete Hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -61,4 +61,23 @@ export class HeroService {
     );
   }
 
+  addHero (hero: Hero):Observable<Hero>{
+    return this.http.post<Hero>(this.heroesUrl, hero,this.httpOptions)
+      .pipe(
+        tap((hero: Hero) => this.log(`added hero w/ id = ${hero.id}`)),
+        catchError(this.handleError<Hero>(`addHero`))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero>{
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id = ${id}`)),
+        catchError(this.handleError<Hero>(`deleteHero`))
+      );
+  }
+
 }
